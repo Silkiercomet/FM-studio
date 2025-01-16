0535fe4107c503a49a8c450783c8e665e9608977
luis Colina <colina.1990lz@gmail.com>
2025-01-16 08:08:14 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const mobile_menu_btn = document.getElementById('mobile_menu_btn');
+const mobile_menu = document.querySelector('.mobile_menu');
+
+mobile_menu_btn.addEventListener('click', () => {
+    mobile_menu.classList.toggle('active');
+    console.log("check")
+});
